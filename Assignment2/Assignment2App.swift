a1c7e6ea883f394ffbb292210bc689aae9df5ff1
LouisLo0818 <22205276@life.hkbu.edu.hk>
2023-11-03 13:25:22 +0800

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by f2205276 on 3/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
